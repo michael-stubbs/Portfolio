3dcc810fe7360b6705d9736fc7db9cac7c80a3e4
Michael Stubbs <michaelstubbsdev@gmail.com>
2021-09-23 14:27:07 -0700

    add styling to initial page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,16 @@ import { Button, ButtonGroup } from "@mui/material";
 
 function Header() {
   return (
-    <div>
-      <ButtonGroup color="primary" variant="text" classname="">
-        <Button>1. About</Button>
-        <Button>2. Experience</Button>
-        <Button>3. Work</Button>
-        <Button>5. Contact</Button>
-      </ButtonGroup>
-      <Button variant="outlined">Resume</Button>
+    <div className="navbar">
+      <div className="BtnGroup">
+        <ButtonGroup color="primary" variant="text" classname="">
+          <Button>1. About</Button>
+          <Button>2. Experience</Button>
+          <Button>3. Work</Button>
+          <Button>5. Contact</Button>
+        </ButtonGroup>
+        <Button variant="outlined">Resume</Button>
+      </div>
     </div>
   );
 }
