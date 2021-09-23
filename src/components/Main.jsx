1c59171f59809e358a5e2e083643d19711d55910
Michael Stubbs <michaelstubbsdev@gmail.com>
2021-09-23 15:27:56 -0700

    add additional styling to Hero

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,16 +3,21 @@ import { Button } from "@mui/material";
 function Main() {
   return (
     <main>
-      <h1 className="">Hi there, my name is</h1>
-      <h2>Michael Stubbs.</h2>
-      <h3>I build professional websites.</h3>
-      <p>
-        I'm a web developer specializing in MERN and Next.js. Currently, I'm
-        focused on offering my services as a freelance developer
-      </p>
-      <Button variant="outlined" size="large">
-        Check out my Upwork!
-      </Button>
+      <div className="jumbotron">
+        <h1 className="">Hi there, my name is</h1>
+        <h2>Michael Stubbs.</h2>
+        <h3>I build professional websites.</h3>
+        <p>
+          I'm a web developer specializing in MERN and Next.js. While I'm
+          currently focused on offering my services as a freelance developer,
+          I'm always looking for my next opportunity!
+        </p>
+        <div className="upwork">
+          <Button variant="outlined" size="large">
+            Check out my Upwork!
+          </Button>
+        </div>
+      </div>
     </main>
   );
 }
