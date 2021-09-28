86f5ea6def6dac2da631576e36ffa2c444bdcd50
Michael Stubbs <michaelstubbsdev@gmail.com>
2021-09-28 13:23:18 -0700

    format navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,25 @@
-import { Button, ButtonGroup } from "@mui/material";
+import { Button, ButtonGroup, createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#EDF5E1",
+    },
+  },
+});
 
 function Header() {
   return (
     <div className="navbar">
       <div className="BtnGroup">
-        <ButtonGroup color="primary" variant="text" classname="">
-          <Button>1. About</Button>
-          <Button>2. Experience</Button>
-          <Button>3. Contact</Button>
-        </ButtonGroup>
-        <Button variant="outlined">Resume</Button>
+        <ThemeProvider theme={theme}>
+          <ButtonGroup color="primary" variant="text" classname="">
+            <Button>1. About</Button>
+            <Button>2. Experience</Button>
+            <Button>3. Contact</Button>
+          </ButtonGroup>
+          <Button variant="outlined">Resume</Button>
+        </ThemeProvider>
       </div>
     </div>
   );
